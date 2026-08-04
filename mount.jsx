9f2ab35cb103f9c7f9f1d4f2ba4921e9c1ebc261
Sahisnu69 <sahisnu6969@gmail.com>
2026-08-04 08:12:42 +0530

    Add files via upload

---
+++ mount.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AttendanceManagerApp from './attendance-manager-app.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AttendanceManagerApp />
+  </React.StrictMode>
+);
