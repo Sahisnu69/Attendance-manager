20aeb508546bba44a655f8ae81e580f369baa442
Sahisnu69 <sahisnu6969@gmail.com>
2026-08-19 19:09:10 +0530

    Delete mount.jsx

--- mount.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AttendanceManagerApp from './attendance-manager-app.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AttendanceManagerApp />
-  </React.StrictMode>
-);
